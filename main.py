2b5a4a85994f13034beb6e597596bcd3080d814a
Lewis <lewis@lappin.me.uk>
2019-02-22 23:01:29 +0000

    added velocity

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import matplotlib.pyplot as plt
 from periodic_lattice import Periodic_Lattice
 from scipy import signal
 import matplotlib.animation as animation
-from matplotlib import style
-
+from scipy import ndimage
 fig = plt.figure()
 ax1 = fig.add_subplot(1,1,1)
+p=0.1
 
 class gameoflife():
     def __init__(self,size,starter,pos=[0,0]):
@@ -16,51 +16,39 @@ class gameoflife():
         self.lattice = Periodic_Lattice(initialArray)
         self.kernel = np.ones((3,3),dtype=int)
         self.kernel[1,1]=0
-        #style.use('fivethirtyeight')
-
-
+        self.CoMs = []
 
     def simulate(self,steps):
         for i in range(0,steps):
-            plt.imshow(self.lattice)
-            self.lattice = self.update(self.lattice)
-            plt.pause(0.01)
-        plt.show()
+            self.lattice = self.update()
+            self.CoMs.append(ndimage.measurements.center_of_mass(np.array(self.lattice)[3:self.N-4,3:self.N-4]))
 
     def update(self):
-        counts = self.NebCounts(self.lattice)
-        updated = ((counts==3)*(self.lattice==0) + (((counts == 2)+ (counts ==3))* self.lattice==1)).astype(int)
+        counts = signal.convolve2d(self.lattice, self.kernel, mode="same",boundary = "wrap")
+        updated = ((counts==3)*(self.lattice==0) + (((counts == 2) + (counts ==3))* self.lattice==1)).astype(int)
         self.lattice=updated
         return updated
 
-    def NebCounts(self,lattice):
-        return signal.convolve2d(lattice, self.kernel, mode="same",boundary = "wrap")
-
-    @classmethod
-    def startRandom(cls,size, p=0.5):
-        print(p)
-        initialArray = np.random.choice(a=[0,1], size=(size, size), p=[p, 1-p])
-        I = cls(size,initialArray)
-        return I
-
     def animate(self,i):
         data = self.update()
-        print(data)
         ax1.clear()
         ax1.imshow(data)
 
 def main():
-    # G = gameoflife(100)
-    # G.simulate(100)
-    # R = gameoflife.startRandom(100)
-    #R.simulate(100)
-    Garray = np.zeros((3,3),dtype=int)
-    Garray[2,:] = 1
-    Garray[1,2] = 1
-    Garray[0,1] = 1
+    #R= gameoflife(100,np.random.choice(a=[0,1], size=(100, 100), p=[p, 1-p]))
+    bee_hive = np.array([[0,1,0],[1,0,1],[1,0,1],[0,1,0]])
+    oscillator = np.array([[1],[1],[1]])
+    glider = np.array([[0,1,0],[0,0,1],[1,1,1]]).T
+    G = gameoflife(50,glider,[10,10])
+    G.simulate(1000)
+    #ani = animation.FuncAnimation(fig, G.animate)
+    CoMs = np.array(G.CoMs)
+    CoMs = np.sqrt(np.power(CoMs[:,0],2)+np.power(CoMs[:,1],2))
+    print(np.nanmean(np.gradient(CoMs)))
+    plt.plot(CoMs)
 
-    G = gameoflife(20,Garray,[10,10])
 
-    ani = animation.FuncAnimation(fig, G.animate)
+    #plt.plot(G.CoMs)
+    #plt.plot(G.CoMs)
     plt.show()
 main()
